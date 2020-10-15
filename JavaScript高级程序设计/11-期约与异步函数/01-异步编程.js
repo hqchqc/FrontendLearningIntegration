dddea05a3b226828b5486bcf934202cf7c3e2787
hqchqc <734986933@qq.com>
2020-10-15 22:00:50 +0800

    22:00 10.15

--- JavaScript高级程序设计/11-期约与异步函数/01-异步编程.js
+++ JavaScript高级程序设计/11-期约与异步函数/01-异步编程.js
@@ -1,28 +1,35 @@
-function double(value,callback){
-    setTimeout(() => {
-        callback(value * 2)
-    }, 1000);
+function double(value) {
+    setTimeout(() => setTimeout(console.log, 0, value * 2), 1000);
 }
 
-double(3, function(x){
-    console.log(`i was given: ${x}`);
-})
+// function double(value, callback) {
+//     setTimeout(() => {
+//         callback(value * 2)
+//     }, 1000);
+// }
 
-function double(value,success,failure){
-    setTimeout(() => {
-        try{
-            if(typeof value !== 'number'){
-                throw 'Must provide number as first arguments';
-            }
-            success(2 * value);
-        } catch(e){
-            failure(e);
-        }
-    }, 1000);
-}
+// double(3, function (x) {
+//     console.log(`i was given: ${x}`);
+// })
 
-const successCallback = x => console.log(`Success: ${x}`);
-const failureCallback = e => console.log(`Failure: ${e}`);
+// function double(value, success, failure) {
+//     setTimeout(() => {
+//         try {
+//             if (typeof value !== 'number') {
+//                 throw 'Must provide number as first arguments';
+//             }
+//             success(2 * value);
+//         } catch (e) {
+//             failure(e);
+//         }
+//     }, 1000);
+// }
 
-double(3,successCallback,failureCallback);
-double('b',successCallback,failureCallback);
+// const successCallback = x => console.log(`Success: ${x}`);
+// const failureCallback = e => console.log(`Failure: ${e}`);
+
+// double(3, successCallback, failureCallback);
+// double('b', successCallback, failureCallback);
+
+let p = new Promise(()=>{});
+setTimeout(console.log, 1000,p);
