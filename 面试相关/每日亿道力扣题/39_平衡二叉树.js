77296f9d85c752232bcffcb1185264abb959e6e0
hqchqc <734986933@qq.com>
2020-10-19 21:19:50 +0800

    21:19 10.19

---
+++ 面试相关/每日亿道力扣题/39_平衡二叉树.js
@@ -0,0 +1,14 @@
+/**
+* 输入一棵二叉树，判断该二叉树是否是平衡二叉树。
+
+在这里，我们只需要考虑其平衡性，不需要考虑其是不是排序二叉树
+*/
+
+/* function TreeNode(x) {
+    this.val = x;
+    this.left = null;
+    this.right = null;
+} */
+function IsBalanced_Solution(pRoot) {
+    
+}
